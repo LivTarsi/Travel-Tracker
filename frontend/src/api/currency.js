379158632f442efe15bfc9c2391f9686ec9fbccb
LivTarsi <143623417+LivTarsi@users.noreply.github.com>
2025-03-19 13:10:09 -0400

    Delete frontend directory

--- frontend/src/api/currency.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'https://api.exchangerate-api.com/v4/latest/';
-
-export const getExchangeRate = async (baseCurrency, targetCurrency) => {
-  const response = await axios.get(`${API_URL}${baseCurrency}`);
-  return response.data.rates[targetCurrency];
-};
-
